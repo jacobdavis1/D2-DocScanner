f85e914cd20c6a474ca108036cf5f326e8439fc1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-02 15:55:07 +0000

    Next test time. Added file uplaoding functionality, should go straight to the user's folder.

--- lib/custom_code/actions/upload_scanned_documents.dart
+++ lib/custom_code/actions/upload_scanned_documents.dart
@@ -6,18 +6,13 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
-import 'dart:html';
+import 'package:dio/dio.dart';
 import 'package:auth0_flutter/auth0_flutter.dart';
 
 Future<void> uploadScannedDocuments(List<String>? documentPaths) async {
   try {
     if (documentPaths != null) {
-      FormData data = new FormData(); // from dart:html
-
-      for (int i = 0; i < documentPaths.length; ++i) {
-        data.append("file" + i.toString(), documentPaths[i]);
-      }
-
+      // Set up Auth0 info
       final Auth0 auth0 =
           Auth0(FFAppState().Auth0ClientDomain, FFAppState().Auth0ClientID);
       final credentials = await auth0.credentialsManager.credentials();
@@ -25,14 +20,22 @@ Future<void> uploadScannedDocuments(List<String>? documentPaths) async {
           "https://doubledfinancial-files.azurewebsites.net/api/file/" +
               (credentials.user.email ?? "");
 
-      HttpRequest.request(endpoint,
-          method: 'POST',
-          sendData: data,
-          requestHeaders: {
-            "Authorization": "Bearer " + credentials.accessToken
-          }).then((HttpRequest r) {
-        // ...
-      });
+      // Construct the file map
+      Map<String, MultipartFile> data = {};
+      for (int i = 0; i < documentPaths.length; ++i) {
+        var mpfile = await MultipartFile.fromFileSync(documentPaths[i]);
+        data.putIfAbsent("file" + i.toString(), () => mpfile);
+      }
+
+      // Construct the form data from the path map
+      final formData = FormData.fromMap({"files": data});
+
+      // Send the request
+      final request = Dio().post(endpoint,
+          data: formData,
+          options: Options(
+            headers: {"Authorization": "Bearer " + credentials.accessToken},
+          ));
     }
   } catch (e, stack) {
     debugPrint(e.toString());
